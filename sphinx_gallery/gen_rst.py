197bc6f7c23c76de3f540652bfb7bab6a71cfd3b
Morten Enemark Lund <melund@gmail.com>
2020-04-17 16:15:22 +0200

    Clean up bad merge

--- sphinx_gallery/gen_rst.py
+++ sphinx_gallery/gen_rst.py
@@ -899,39 +899,44 @@ def save_rst_example(example_rst, example_file, time_elapsed,
     ref_fname = os.path.relpath(example_file, gallery_conf['src_dir'])
     ref_fname = ref_fname.replace(os.path.sep, "_")
 
-    if gallery_conf['show_code_section']:
-      binder_conf = check_binder_conf(gallery_conf.get('binder'))
+    if not gallery_conf['show_code_section']:
+        example_rst = (".. only:: html\n\n"
+                        "    .. rst-class:: sphx-glr-example-title\n\n"
+                        "    .. _sphx_glr_{0}:\n\n"
+                        ).format(ref_fname) + example_rst
+    else:
+        binder_conf = check_binder_conf(gallery_conf.get('binder'))
 
-      binder_text = (" or to run this example in your browser via Binder"
-                     if len(binder_conf) else "")
-      example_rst = (".. only:: html\n\n"
-                     "    .. note::\n"
-                     "        :class: sphx-glr-download-link-note\n\n"
-                     "        Click :ref:`here <sphx_glr_download_{0}>` "
-                     "    to download the full example code{1}\n"
-                     "    .. rst-class:: sphx-glr-example-title\n\n"
-                     "    .. _sphx_glr_{0}:\n\n"
-                     ).format(ref_fname, binder_text) + example_rst
+        binder_text = (" or to run this example in your browser via Binder"
+                        if len(binder_conf) else "")
+        example_rst = (".. only:: html\n\n"
+                        "    .. note::\n"
+                        "        :class: sphx-glr-download-link-note\n\n"
+                        "        Click :ref:`here <sphx_glr_download_{0}>` "
+                        "    to download the full example code{1}\n"
+                        "    .. rst-class:: sphx-glr-example-title\n\n"
+                        "    .. _sphx_glr_{0}:\n\n"
+                        ).format(ref_fname, binder_text) + example_rst
 
-      if time_elapsed >= gallery_conf["min_reported_time"]:
-          time_m, time_s = divmod(time_elapsed, 60)
-          example_rst += TIMING_CONTENT.format(time_m, time_s)
-      if gallery_conf['show_memory']:
-          example_rst += ("**Estimated memory usage:** {0: .0f} MB\n\n"
-                          .format(memory_used))
+        if time_elapsed >= gallery_conf["min_reported_time"]:
+            time_m, time_s = divmod(time_elapsed, 60)
+            example_rst += TIMING_CONTENT.format(time_m, time_s)
+        if gallery_conf['show_memory']:
+            example_rst += ("**Estimated memory usage:** {0: .0f} MB\n\n"
+                            .format(memory_used))
 
-      # Generate a binder URL if specified
-      binder_badge_rst = ''
-      if len(binder_conf) > 0:
-          binder_badge_rst += gen_binder_rst(example_file, binder_conf,
-                                             gallery_conf)
+        # Generate a binder URL if specified
+        binder_badge_rst = ''
+        if len(binder_conf) > 0:
+            binder_badge_rst += gen_binder_rst(example_file, binder_conf,
+                                                gallery_conf)
 
-      fname = os.path.basename(example_file)
-      example_rst += CODE_DOWNLOAD.format(fname,
-                                          replace_py_ipynb(fname),
-                                          binder_badge_rst,
-                                          ref_fname)
-      example_rst += SPHX_GLR_SIG
+        fname = os.path.basename(example_file)
+        example_rst += CODE_DOWNLOAD.format(fname,
+                                            replace_py_ipynb(fname),
+                                            binder_badge_rst,
+                                            ref_fname)
+        example_rst += SPHX_GLR_SIG
 
     write_file_new = re.sub(r'\.py$', '.rst.new', example_file)
     with codecs.open(write_file_new, 'w', encoding="utf-8") as f:
